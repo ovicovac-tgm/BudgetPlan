ce3cb59c80ae88fe873700447d5631f76aa2cde4
ovicovac-tgm <ovicovac@student.tgm.ac.at>
2018-02-27 00:29:43 +0100

    Initial commit

---
+++ BudgetApp.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from Tabelle import *
+from Eingabefeld import *
+
+
+class Application(Frame):
+
+    def __init__(self):
+        self.root = Tk()
+        #self.root.resizable(False, False)
+        Frame.__init__(self)
+        self.pack
+        self.createWidgets()
+        self.Window()
+
+    def Window(self):
+        table = Table(self.root, ["Zweck", "Betrag in Euro", "Anmerkung"], column_minwidths=[None, None, None])
+        table.pack(expand=True, fill=X, padx=10, pady=10)
+        table.set_data([[1, 1, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+        table.cell(0, 0, "")
+        table.cell(1, 1, "Hi")
+        self.root.title("Kostenplan")
+        width = 700
+        height = 500
+
+        screenWidth = self.root.winfo_screenwidth()
+        screenHeight = self.root.winfo_screenheight()
+
+        x_coordinate = (screenWidth / 2) - (width / 2)
+        y_coordinate = (screenHeight / 2) - (height / 2)
+
+        self.root.geometry("%dx%d+%d+%d" % (width, height, x_coordinate, y_coordinate))
+
+        mainloop()
+        self.root.destroy()
+
+    def createWidgets(self):
+        # Buttons
+        self.erstellen = Button(self.root, text="Eintrag hinzufügen", bg="#5e5e5e", fg="#a1dbcd", height=2, width=100, command = self.EingabeWindow)
+
+
+        # hinzufügen
+        self.erstellen.pack(side=BOTTOM)
+
+    def EingabeWindow(self):
+        Eingabe()
+
+
+Application()
---
+++ Eingabefeld.py
@@ -0,0 +1,52 @@
+from tkinter import *
+
+class Eingabe(Frame):
+
+    def __init__(self):
+        self.rootEingabe = Tk()
+        self.rootEingabe.resizable(False, False)
+        Frame.__init__(self)
+        self.pack
+        self.createEingabeWidgets()
+        self.showEingabe()
+
+    def showEingabe(self):
+        self.rootEingabe.title("Eintrag hinzufügen")
+        width = 500
+        height = 200
+
+        screenWidth = self.rootEingabe.winfo_screenwidth()
+        screenHeight = self.rootEingabe.winfo_screenheight()
+
+        x_coordinate = (screenWidth / 2) - (width / 2)
+        y_coordinate = (screenHeight / 2) - (height / 2)
+
+        self.rootEingabe.geometry("%dx%d+%d+%d" % (width, height, x_coordinate, y_coordinate))
+
+        mainloop()
+        self.rootEingabe.destroy()
+
+    def createEingabeWidgets(self):
+        #Buttons
+        ready = Button(self.rootEingabe, text="Fertig", bg="#5e5e5e", fg="#a1dbcd", height=2, width=10)
+
+        #Label/Entry
+        l1 = Label(self.rootEingabe, text="Zweck", height = 2)
+        l2 = Label(self.rootEingabe, text="Betrag in Euro", height = 2)
+        l3 = Label(self.rootEingabe, text="Anmerkung", height = 2)
+
+        zEntry = Entry(self.rootEingabe, width= 60)
+        bEntry = Entry(self.rootEingabe, width= 60)
+        aEntry = Entry(self.rootEingabe, width= 60)
+
+        l1.grid(row =0)
+        l2.grid(row=1)
+        l3.grid(row=2)
+
+        zEntry.grid(row=0, column=1)
+        bEntry.grid(row=1, column=1)
+        aEntry.grid(row=2, column=1)
+        ready.grid(row = 3, column=1)
+
+        #hinzufügen
+
